a61926584487b9137ffcabf83a8ed14de9640e89
Diogo Biazus <diogo@biazus.ca>
2022-04-22 09:43:15 -0400

    There is some issue in the current cloudflare adapter that prevents formData from being properly transformed in URLSearchParams

--- src/input-resolvers.ts
+++ src/input-resolvers.ts
@@ -4,7 +4,9 @@ const inputFromSearch = (queryString: URLSearchParams) =>
   qs.parse(queryString.toString())
 
 const inputFromFormData = (formData: FormData) =>
-  inputFromSearch(new URLSearchParams(formData as URLSearchParams))
+  inputFromSearch(
+    new URLSearchParams(formData.entries() as unknown as URLSearchParams),
+  )
 
 const inputFromForm = async (request: Request) =>
   inputFromFormData(await request.clone().formData())
